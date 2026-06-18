152037ecd8dc96e50bd1f2cd7ad00e8afbe9065e
leonardodextercorrea-cpu <leonardo.dexter.correa@escola.pr.gov.br>
2026-06-18 19:47:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let score = 0;
 
+/* MODO ESCURO */
+function toggleDark(){
+    document.body.classList.toggle("dark");
+}
+
+/* QUIZ */
+function answer(correct){
+    if(correct){
+        score++;
+        document.getElementById("result").innerText = "✔ Correto!";
+    } else {
+        document.getElementById("result").innerText = "❌ Tente novamente!";
+    }
+
+    document.getElementById("score").innerText = score;
+}
